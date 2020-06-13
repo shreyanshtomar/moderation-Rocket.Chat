61d1904ca0b097766a28454ccf4b96f5f1184e27
jsobrepe <jsobrepe@example.com>
2020-06-13 12:30:00 +0800

    Updated .gitignore and test files

--- tests/inference_test.py
+++ tests/inference_test.py
@@ -1,11 +1,15 @@
 import requests, unittest, base64
 from server.server import app
 
+import requests
+from io import BytesIO
+
 class BasicTestCase(unittest.TestCase):
+    test_image_directory = 'tests'
 
     def test_sfw_image(self):
         tester = app.test_client(self)
-        path = 'test_images/park.jpeg'
+        path = f"{self.test_image_directory}/test_images/park.jpeg"
         resp = tester.post('/predict', data={"file": open(path,'rb')})
 
         self.assertEqual(resp.status_code, 200)
@@ -13,7 +17,7 @@ class BasicTestCase(unittest.TestCase):
 
     def test_sfw_image_sent_as_base64(self):
         tester = app.test_client(self)
-        image_path = 'test_images/park.jpeg'
+        image_path = f"{self.test_image_directory}/test_images/park.jpeg"
         image_file_handle = open(image_path, "rb")
         base64_string = base64.b64encode(image_file_handle.read())
         resp = tester.post('/predict', data={"file": base64_string})
